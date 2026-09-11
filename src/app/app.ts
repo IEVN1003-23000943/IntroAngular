d3a673101b3228a049e1bc39783c824377e60274
IEVN1003-23000943 <82707@alumnos.utleon.edu.mx>
2026-09-10 21:00:01 -0600

    heroes 10/09/2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,17 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('IntroAngular');
+
+  title:string = 'Josué Saúl Ramírez Jaramillo'
+
+  duplicarNumero(a:number):number{
+    return a*2
+  }
+
+  pelicula ={
+    titulo: 'Spider-Man',
+    fechaLanzamiento: new Date(),
+    precio: 2355
+  }
+
 }
