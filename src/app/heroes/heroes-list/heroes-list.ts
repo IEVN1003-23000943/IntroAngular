6be5f9f3e65784a2f1240903d6dac3a7a3aa7ac5
IEVN1003-23000943 <82707@alumnos.utleon.edu.mx>
2026-09-11 18:52:56 -0600

    FormsModules 11/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,52 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+  imageWidth: number = 40;
+  imageMargin: number = 2;
+  muestraImagen: boolean = true;
+  listfilter: string = '';
+
+  showImage():void{
+    this.muestraImagen=!this.muestraImagen;
+  }
+
+  heroes:any[]=[
+    {
+      imagen: "https://dragonball-api.com/characters/picolo_normal.webp",
+      nombre: "Piccolo",
+      description: "Makankosapo",
+      race: "Namekiano",
+      ki: 7000
+    },
+    {
+      imagen: "https://dragonball-api.com/characters/Freezer.webp",
+      nombre: "Frezzer",
+      description: "Death Ball",
+      race: "Demon frice",
+      ki: 1000000
+    },
+    {
+      imagen: "https://dragonball-api.com/characters/goku_normal.webp",
+      nombre: "Goku",
+      description: "Kamehameha",
+      race: "Sayayin",
+      ki: 1200000
+    },
+    {
+      imagen: "https://dragonball-api.com/characters/vegeta_normal.webp",
+      nombre: "Vegeta",
+      description: "Resplandor final",
+      race: "Sayayin",
+      ki: 800000
+    },
+    {
+      imagen: "https://dragonball-api.com/characters/gohan.webp",
+      nombre: "Gohan",
+      description: "kamehameha",
+      race: "Human-Sayan",
+      ki: 1500000
+    },
+  ]
+}
